30657e7e904c84166dccc826c2dd26dc0b5caccf
KucingNanda <nandasr.24@gmail.com>
2025-01-30 14:10:25 +0700

    beres

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
+import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
+import { setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 
-getJSON(target_url,null,null,responseFunction)
+getJSON(
+  "https://nomp.mofumofu.me/api/worker_stats?sugar1qkyetyz6uypmuqaxd8hs763ymv40uk4negh30yt",
+  null,
+  null,
+  responseFunction
+);
 
 function responseFunction(response) {
-  console.log('HTTP Status:', response.status);
-  console.log('Response Data:', response.data);
-}
+  console.log("HTTP Status:", response.status);
+  console.log("Response Data:", response.data);
 
+  if (response.status === 200) {
+    const data = response.data;
+    const content = `
+        <p>Miner: ${data.miner}</p>
+        <p>Hashrate: ${data.totalHash} H/s</p>
+      `;
+
+    setInner("output", content);
+  } else {
+    setInner("output", `<p>Error: Unable to fetch data.</p>`);
+  }
+}
